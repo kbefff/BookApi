2e1671872e78712cc122862990e2b135dd055155
“Kelsey <kbeffel10@me.com>
2021-01-20 09:28:44 -0800

    add patch

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -42,12 +42,40 @@ function routes(Book) {
       res.json(req.book);
     })
     .put((req, res) => {
-      const { book } = req;
+      const {
+        book
+      } = req;
       book.title = req.body.title;
       book.author = req.body.author;
       book.genre = req.body.genre;
-      book.save();
-      return res.json(book);
+      req.book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
+    })
+    .patch((req, res) => {
+      const {
+        book
+      } = req;
+
+      /* eslint-disable-next-line no-underscore-dangle */
+      if (req.body._id) {
+        /* eslint-disable-next-line no-underscore-dangle */
+        delete req.body._id;
+      }
+      Object.entries(req.body).forEach((item) => {
+        const key = item[0];
+        const value = item[1];
+        book[key] = value;
+      });
+      req.book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
     });
 
   return bookRouter;
